0a45b8f3179be7a936d3fef4fae99e4bdec06c80
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-01-11 20:05:30 -0500

    All get and delete routes are now filled out

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,27 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [{ model: Product }],
+  })
+  .then((categoryData) => {
+    res.json(categoryData);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id)
+  .then((categoryData) => {
+    res.json(categoryData);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
@@ -23,6 +39,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deletedCategory) => {
+    res.json(deletedCategory);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,27 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{ model: Product }],
+  })
+  .then((tagData) => {
+    res.json(tagData);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id)
+  .then((tagData) => {
+    res.json(tagData);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
@@ -23,6 +39,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deletedTag) => {
+    res.json(deletedTag);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;
